fa279a7e8d07d2ad2dbf40decd378b1c1ce37e5b
der10pm <der10pm@gmail.com>
2020-05-07 18:25:23 +0200

    Initial commit

---
+++ TeleporterCharging/ChargeInHalfPlugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+
+namespace TeleporterCharging
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("der10pm.chargeinhalf", "Charge in half", "1.0.0.0")]
+    public class ChargeInHalfPlugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            if (RoR2.Run.instance.stageClearCount > RoR2.Run.stagesPerLoop) {
+                On.RoR2.HoldoutZoneController.OnEnable += (orig, self) =>
+                {
+                    self.baseChargeDuration /= RoR2.Run.instance.stageClearCount / RoR2.Run.stagesPerLoop;
+                    orig(self);
+                };
+            }
+        }
+    }
+}

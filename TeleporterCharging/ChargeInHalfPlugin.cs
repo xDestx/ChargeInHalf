f2b18ef577a5ec70ad06b43d5ae870ef4c159dfa
der10pm <der10pm@gmail.com>
2020-05-09 18:42:35 +0200

    Fix charging reduction and derp

--- TeleporterCharging/ChargeInHalfPlugin.cs
+++ TeleporterCharging/ChargeInHalfPlugin.cs
@@ -3,18 +3,16 @@
 namespace TeleporterCharging
 {
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("der10pm.chargeinhalf", "Charge in half", "1.0.0.0")]
+    [BepInPlugin("der10pm.chargeinhalf", "ChargeInHalf", "1.0.1")]
     public class ChargeInHalfPlugin : BaseUnityPlugin
     {
         void Awake()
         {
-            if (RoR2.Run.instance.stageClearCount > RoR2.Run.stagesPerLoop) {
-                On.RoR2.HoldoutZoneController.OnEnable += (orig, self) =>
-                {
-                    self.baseChargeDuration /= RoR2.Run.instance.stageClearCount / RoR2.Run.stagesPerLoop;
-                    orig(self);
-                };
-            }
+            On.RoR2.HoldoutZoneController.OnEnable += (orig, self) =>
+            {
+                self.baseChargeDuration /= (RoR2.Run.instance.stageClearCount + 5) / RoR2.Run.stagesPerLoop;
+                orig(self);
+            };
         }
     }
 }
